05af9646b9bf2dfcc0b4aca6f7d970d792a8ec41
Antonio Willy Malara <antonio.malara@gmail.com>
2016-06-21 00:07:43 +0200

    made the thing link

---
+++ nanotests/nanotests/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  nanotests
+//
+//  Created by Antonio Malara on 20/06/16.
+//  Copyright © 2016 Antonio Malara. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ nanotests/nanotests/nanotests-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#include "nn.h"
+#include "pubsub.h"
+#include "pipeline.h"
+#include "bus.h"
+#include "pair.h"
+#include "survey.h"
+#include "reqrep.h"
